c63c6f257958343232d20f361cdd5fb3e68efaa0
El Moujoud Hanane <133366825+HannElmjd@users.noreply.github.com>
2025-02-04 13:19:54 +0000

    fix: update fonction in "mon_premier_script.py"

--- courses/mon_premier_script.py
+++ courses/mon_premier_script.py
@@ -1,23 +1,28 @@
 import unittest
 
-"""
-Count names with more than seven letters
-"""
-def names(prenoms):
-    more_than_seven = 0
-    for prenom in prenoms:
-        if len(prenom) > 7:
-            more_than_seven += 1
-            print(prenom + " est un prénom avec un nombre de lettres supérieur à 7")
-        else:
-            print(prenom + " est un prénom avec un nombre de lettres inférieur ou égal à 7")
-    return more_than_seven
+def count_long_names(first_names, threshold=7):
+    """ Compte le nombre de prénoms ayant plus de 'threshold' 'seuil'lettres et affiche le résultat """
+    count = len([name for name in first_names if len(name) > threshold])
 
-class TestNamesMethod(unittest.TestCase):
-     def test_names(self):
-        prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
-        more_than_seven = names(prenoms=prenoms)
-        self.assertEqual(more_than_seven, 4)
+    for name in first_names:
+        if len(name) > threshold:
+            comparison = "supérieur"
+        else:
+            comparison = "inférieur ou égal"
+        print(name + " est un prénom avec un nombre de lettres " + comparison + " à " + str(threshold))
+    return count
+
+class TestCountLongNames(unittest.TestCase):
+    def test_default_threshold(self):
+        first_names = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
+        self.assertEqual(count_long_names(first_names), 4)
+
+    def test_custom_threshold(self):
+        first_names = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
+        self.assertEqual(count_long_names(first_names, threshold=8), 2)
+
+    def test_empty_list(self):
+        self.assertEqual(count_long_names([]), 0)
 
 if __name__ == '__main__':
     unittest.main()
